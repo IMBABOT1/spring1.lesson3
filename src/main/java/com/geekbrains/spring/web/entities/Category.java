0529edd364f2645e8e4529952792175b55ecc30c
unknown <zlovred12@gmail.com>
2022-11-02 22:02:18 +0300

    temp

--- src/main/java/com/geekbrains/spring/web/entities/Category.java
+++ src/main/java/com/geekbrains/spring/web/entities/Category.java
@@ -1,16 +1,26 @@
 package com.geekbrains.spring.web.entities;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "title")
     private String title;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @OneToMany(mappedBy = "category")
     private List<Product> list;
 
 }
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,36 +1,48 @@
+create table categories
+(
+    id       bigserial primary key,
+    title    varchar(255)
+);
+
+insert into categories (title)
+values ('games'),
+       ('products');
+
 create table products
 (
     id         bigserial primary key,
     title      varchar(255),
     price      int,
+    category_id bigint REFERENCES categories (id),
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
 
 
-INSERT INTO products (title, price) VALUES
-                                        ('Dark Souls', 80),
-                                        ('Dark Souls2', 80),
-                                        ('Dark Souls3', 80),
-                                        ('Bloodborne', 80),
-                                        ('Sekiro', 80),
-                                        ('Elden Ring', 80),
-                                        ('StarCraft', 80),
-                                        ('World of Warcraft ', 80),
-                                        ('World of Warcraft: Wrath of the Lich King', 80),
-                                        ('StarCraft II', 80),
-                                        ('StarCraft II Wings of Liberty', 80),
-                                        ('StarCraft II: Heart of the Swarm', 80),
-                                        ('StarCraft II: Legacy of the Void', 80),
-                                        ('Dota', 80),
-                                        ('Dota 2', 80),
-                                        ('World of Warcraft: Burning Crusade', 80),
-                                        ('World of Warcraft: Cataclysm', 80),
-                                        ('World of Warcraft: Mists of Pandaria', 80),
-                                        ('World of Warcraft: Warlords of Draenor', 80),
-                                        ('Milk', 100),
-                                        ('Bread', 80),
-                                        ('Cheese', 90);
+INSERT INTO products (title, price, category_id) VALUES
+                                                     ('Dark Souls', 80, 1),
+                                                     ('Dark Souls2', 80, 1),
+                                                     ('Dark Souls3', 80, 1),
+                                                     ('Bloodborne', 80, 1),
+                                                     ('Sekiro', 80, 1),
+                                                     ('Elden Ring', 80, 1),
+                                                     ('StarCraft', 80, 1),
+                                                     ('World of Warcraft ', 80, 1),
+                                                     ('World of Warcraft: Wrath of the Lich King', 80, 1),
+                                                     ('StarCraft II', 80, 1),
+                                                     ('StarCraft II Wings of Liberty', 80, 1),
+                                                     ('StarCraft II: Heart of the Swarm', 80, 1),
+                                                     ('StarCraft II: Legacy of the Void', 80, 1),
+                                                     ('Dota', 80, 1),
+                                                     ('Dota 2', 80, 1),
+                                                     ('World of Warcraft: Burning Crusade', 80, 1),
+                                                     ('World of Warcraft: Cataclysm', 80, 1),
+                                                     ('World of Warcraft: Mists of Pandaria', 80, 1),
+                                                     ('World of Warcraft: Warlords of Draenor', 80, 1),
+                                                     ('Milk', 100, 2),
+                                                     ('Bread', 80, 2),
+                                                     ('Cheese', 90, 2);
+
 
 
 create table users
@@ -43,34 +55,12 @@ create table users
     updated_at timestamp default current_timestamp
 );
 
-create table roles
-(
-    id         bigserial primary key,
-    name       varchar(50) not null,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
-);
 
-create table users_roles
-(
-    user_id    bigint not null references users (id),
-    role_id    bigint not null references roles (id),
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp,
-    primary key (user_id, role_id)
-);
-
-insert into roles (name)
-values ('ROLE_USER'),
-       ('ROLE_ADMIN');
 
 insert into users (username, password, email)
 values ('bob', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'bob_johnson@gmail.com'),
        ('john', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'john_johnson@gmail.com');
 
-insert into users_roles (user_id, role_id)
-values (1, 1),
-       (2, 2);
 
 create table orders
 (
@@ -82,6 +72,10 @@ create table orders
     created_at  timestamp default current_timestamp,
     updated_at  timestamp default current_timestamp
 );
+
+insert into orders (user_id, total_price, address, phone)
+values (1, 200, 'address', '12345');
+
 
 create table order_items
 (
@@ -95,11 +89,41 @@ create table order_items
     updated_at        timestamp default current_timestamp
 );
 
-insert into orders (user_id, total_price, address, phone)
-values (1, 200, 'address', '12345');
-
 insert into order_items (product_id, order_id, quantity, price_per_product, price)
 values (1, 1, 2, 100, 200);
+
+
+create table roles
+(
+    id         bigserial primary key,
+    name       varchar(50) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
+
+
+insert into roles (name)
+values ('ROLE_USER'),
+       ('ROLE_ADMIN');
+
+create table users_roles
+(
+    user_id    bigint not null references users (id),
+    role_id    bigint not null references roles (id),
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    primary key (user_id, role_id)
+);
+
+insert into users_roles (user_id, role_id)
+values (1, 1),
+       (2, 2);
+
+
+
+
+
+
 
 
 
