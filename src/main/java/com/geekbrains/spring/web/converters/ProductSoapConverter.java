dc53a27b9bf190d45c4ed8ff17c301ab096fece2
unknown <zlovred12@gmail.com>
2022-11-08 22:06:41 +0300

    temp

---
+++ src/main/java/com/geekbrains/spring/web/converters/ProductSoapConverter.java
@@ -0,0 +1,19 @@
+package com.geekbrains.spring.web.converters;
+
+
+import com.geekbrains.spring.web.dto.ProductDto;
+import com.geekbrains.spring.web.entities.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductSoapConverter {
+
+
+    public com.geekbrains.spring.web.soap.Product entityToDto(Product product) {
+        com.geekbrains.spring.web.soap.Product p = new com.geekbrains.spring.web.soap.Product();
+        p.setId(product.getId());
+        p.setPrice(product.getPrice());
+        p.setTitle(product.getTitle());
+        return p;
+    }
+}
--- src/main/java/com/geekbrains/spring/web/endpoints/ProductEndpoint.java
+++ src/main/java/com/geekbrains/spring/web/endpoints/ProductEndpoint.java
@@ -1,46 +1,66 @@
 package com.geekbrains.spring.web.endpoints;
 
+
+import com.geekbrains.spring.web.converters.ProductSoapConverter;
+import com.geekbrains.spring.web.entities.Product;
 import com.geekbrains.spring.web.services.ProductsService;
+import com.geekbrains.spring.web.soap.GetAllProductsRequest;
+import com.geekbrains.spring.web.soap.GetAllProductsResponse;
 import com.geekbrains.spring.web.soap.GetProductByIdRequest;
 import com.geekbrains.spring.web.soap.GetProductByIdResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+
+import java.util.List;
+import java.util.Optional;
 
 @Endpoint
 @RequiredArgsConstructor
 public class ProductEndpoint {
     private static final String NAMESPACE_URI = "http://www.imbabot.com/spring/ws/products";
     private final ProductsService productsService;
+    private final ProductSoapConverter productSoapConverter;
+
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductByIdRequest")
     @ResponsePayload
-    public GetProductByIdResponse getStudentByName(@RequestPayload GetProductByIdRequest request) {
+    public GetProductByIdResponse getProductById(@RequestPayload GetProductByIdRequest request) {
         GetProductByIdResponse response = new GetProductByIdResponse();
-        response.setProduct(productsService.findById(request.getId()));
+        Optional<Product> optional = productsService.findById(request.getId());
+
+        if (optional.isPresent()) {
+            response.setProduct(productSoapConverter.entityToDto(optional.get()));
+        }
         return response;
     }
 
-    /*
-        Пример запроса: POST http://localhost:8080/ws
-        Header -> Content-Type: text/xml
 
-        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:f="http://www.flamexander.com/spring/ws/students">
-            <soapenv:Header/>
-            <soapenv:Body>
-                <f:getAllStudentsRequest/>
-            </soapenv:Body>
-        </soapenv:Envelope>
-     */
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllStudentsRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllProductsRequest")
     @ResponsePayload
-    public GetAllStudentsResponse getAllStudents(@RequestPayload GetAllStudentsRequest request) {
-        GetAllStudentsResponse response = new GetAllStudentsResponse();
-        studentService.getAllStudents().forEach(response.getStudents()::add);
+    public GetAllProductsResponse getAllProducts(@RequestPayload GetAllProductsRequest request,
+                                                 @RequestParam(name = "p", defaultValue = "1") Integer page,
+                                                 @RequestParam(name = "min_price", required = false) Integer minPrice,
+                                                 @RequestParam(name = "max_price", required = false) Integer maxPrice,
+                                                 @RequestParam(name = "title_part", required = false) String titlePart,
+                                                 @RequestParam(name = "category", required = false) String category
+
+
+    ) {
+        GetAllProductsResponse response = new GetAllProductsResponse();
+       // studentService.getAllStudents().forEach(response.getStudents()::add);
+        Page<Product> optionals = productsService.findAll(minPrice, maxPrice, titlePart, category, page);
+        for (Product p : optionals.toList()){
+            response.getProducts().add(productSoapConverter.entityToDto(p));
+        }
         return response;
     }
+
+
 }
 
